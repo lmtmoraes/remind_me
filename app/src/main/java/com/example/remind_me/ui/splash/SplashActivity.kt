4d08fdc9af321422551d5195a96d864a429aa254
Lucas Moraes <lmt.lucasmoraesteixeira@gmail.com>
2023-03-27 21:35:43 -0300

    splash activity criada

---
+++ app/src/main/java/com/example/remind_me/ui/splash/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.remind_me.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.remind_me.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater).apply { setContentView(root) }
+    }
+}
