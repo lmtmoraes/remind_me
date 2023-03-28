7ee77e6b2558a775c9780b37c542cfa61f2546a5
Lucas Moraes <lmt.lucasmoraesteixeira@gmail.com>
2023-03-27 22:15:59 -0300

    splash activity finalizada

--- app/src/main/java/com/example/remind_me/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/remind_me/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.remind_me
+package com.example.remind_me.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.remind_me.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/remind_me/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/remind_me/ui/splash/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.example.remind_me.ui.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.remind_me.databinding.ActivitySplashBinding
+import com.example.remind_me.ui.main.MainActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -13,5 +17,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater).apply { setContentView(root) }
+        
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finishAffinity()
+        }, 3000)
     }
 }
